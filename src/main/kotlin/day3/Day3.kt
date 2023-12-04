ed0a0e757ff56ff6c5eb7911de779679d8d91923
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-04 05:55:24 +0100

    so far

--- src/main/kotlin/day3/Day3.kt
+++ src/main/kotlin/day3/Day3.kt
@@ -9,151 +9,58 @@ fun main() {
 }
 
 object Day3 : Challenge() {
-    val parsed = input.lines().withIndex().flatMap { (y, line) ->
-        line.withIndex().map { (x, char) ->
-            (y to x) to char
-        }
-    }.toMap()
-
-    sealed interface Segment
     data class Number(
-        val startPos: Pair<Int, Int>,
-        val number: Int,
-        val isPart: Boolean,
+        override val pos: Pair<Int, Int>,
+        var value: Int,
     ) : Segment
 
+    sealed interface Segment {
+        val pos: Pair<Int, Int>
+    }
+
     data class Part(
-        val pos: Pair<Int, Int>,
+        override val pos: Pair<Int, Int>,
         val kind: Char,
     ) : Segment
 
-    override fun part1(): Any? {
-        val parts = buildMap {
-            var pos = 0 to 0
-            var number = 0
-            var isPart = false
-            parsed.entries.forEach { (point, char) ->
+    val grid = buildMap<Pair<Int, Int>, Segment> {
+        input.lines().forEachIndexed { y, line ->
+            line.forEachIndexed { x, char ->
+                val pos = y to x
                 if (char.isDigit()) {
-                    number *= 10
-                    number += char.digitToInt()
-                    if (isAdjacent(point)) {
-                        isPart = true
+                    val digit = char.digitToInt()
+                    when (val leftNeighbour = get(pos + (0 to -1)) as? Number) {
+                        null -> put(pos, Number(pos, digit))
+                        else -> put(
+                            pos,
+                            leftNeighbour.apply {
+                                value = value * 10 + char.digitToInt()
+                            },
+                        )
                     }
-                }
-                if (!char.isDigit()) {
-                    if (number != 0) {
-                        put(pos, Number(pos, number, isPart))
-                        pos = point
-                        number = 0
-                        isPart = false
-                    }
+                } else if (char != '.') {
+                    put(pos, Part(pos, char))
                 }
             }
         }
-        return parts.entries.filter { (key, value) ->
-            value.isPart
-        }.sumOf { (key, value) -> value.number }
     }
+    val numbers = grid.values.distinct().filterIsInstance<Number>()
+    val parts = grid.values.distinct().filterIsInstance<Part>()
 
-//    fun buildNumber(pos: Pair<Int, Int>, char: Char): Int {
-//        var startPos = pos
-//        while (true) {
-//            startPos += (0 to -1)
-//            if (parsed[startPos] == null) {
-//                break
-//            }
-//            if (!parsed[startPos]!!.isDigit()) {
-//                break
-//            }
-//        }
-//        startPos += (0 to 1)
-//        var number = parsed[startPos]!!.takeIf { it.isDigit() }?.digitToInt() ?: return 0
-//        while (true) {
-//            startPos += (0 to 1)
-//            if (parsed[startPos] == null) {
-//                break
-//            }
-//            if (!parsed[startPos]!!.isDigit()) {
-//                break
-//            }
-//            number = number * 10 + parsed.getValue(startPos).digitToInt()
-//        }
-//        return number
-//    }
-
-    fun isAdjacent(point: Pair<Int, Int>): Boolean {
-        val adjacency = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
-        return adjacency.any {
-            val character = parsed[point + it]
-            when (character) {
-                null -> false
-                else -> when {
-                    character.isDigit() -> false
-                    character == '.' -> false
-                    else -> true
-                }
-            }
+    override fun part1(): Any? {
+        numbers.filter {
+            val neighbours = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
+            val otherPos = i
+            true
         }
     }
 
     operator fun Pair<Int, Int>.plus(other: Pair<Int, Int>): Pair<Int, Int> =
         first + other.first to second + other.second
 
-    // operator fun Pair<Int, Int>.minus(other: Pair<Int, Int>): Pair<Int, Int> = first - other.first to second - other.second
+// operator fun Pair<Int, Int>.minus(other: Pair<Int, Int>): Pair<Int, Int> = first - other.first to second - other.second
 
     override fun part2(): Any? {
-        val parts = buildMap<Pair<Int, Int>, Segment> {
-            var pos = 0 to -1
-            var number = 0
-            var isPart = false
-            var prevWasntNumber = true
-            parsed.entries.forEach { (point, char) ->
-                if (char.isDigit()) {
-                    if (prevWasntNumber) {
-                        pos = point
-                    }
-                    prevWasntNumber = false
-                    number *= 10
-                    number += char.digitToInt()
-                    if (isAdjacent(point)) {
-                        isPart = true
-                    }
-                }
-                if (!char.isDigit()) {
-                    prevWasntNumber = true
-                    if (number != 0) {
-                        val numberClass = Number(pos, number, isPart)
-                        put(pos, Number(pos, number, isPart))
-                        val numberLength = number.toString().length
-                        if (numberLength > 1) {
-                            put(pos + (0 to 1), numberClass)
-                        }
-                        if (numberLength > 2) {
-                            put(pos + (0 to 2), numberClass)
-                        }
-                    }
-                    if (char != '.') {
-                        put(point, Part(point, char))
-                    }
-                    number = 0
-                    isPart = false
-                }
-            }
-        }
-        return parts.filter { (key, value) -> value is Part }.map { (key, value) ->
-            value as Part
-            if (value.kind != '*') {
-                return@map 0
-            }
-            println(key to value)
-            val neighbours = listOf(-1 to -1, -1 to 0, -1 to 1, 0 to -1, 0 to 1, 1 to -1, 1 to 0, 1 to 1)
-            val toReturn = neighbours.mapNotNull {
-                parts[key + it] as? Number
-            }.distinct().takeIf { it.size == 2 }?.let { (a, b) ->
-                println(a to b)
-                a.number * b.number
-            } ?: 0
-            toReturn
-        }.sum()
+        return null
     }
 }
