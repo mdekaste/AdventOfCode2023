d9a906fea3e136e0bb04f61a7dcb92d8d8a43ee0
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-08 08:07:27 +0100

    final cleanup

--- src/main/kotlin/day8/Day8.kt
+++ src/main/kotlin/day8/Day8.kt
@@ -13,41 +13,37 @@ object Day8 : Challenge() {
     val graph: Map<String, Pair<String, String>>
 
     init {
-        val parsed = input.splitOnEmpty().let { (lr, commands) ->
-            lr.toList() to commands.lines().map { line ->
+        input.splitOnEmpty().let { (lr, commands) ->
+            path = sequence { while (true) yieldAll(lr.toList()) }
+            graph = commands.lines().map { line ->
                 line.split(" = ").let { (a, b) ->
-                    a to (b.substring(1).substringBefore(')').split(", ").let { (x, y) -> x to y })
+                    a to (b.substringAfter('(').substringBefore(')').split(", ").let { (x, y) -> x to y })
                 }
             }.toMap()
         }
-        path = sequence { while (true) yieldAll(parsed.first) }
-        graph = parsed.second
     }
 
-    fun solve(startPositions: (String) -> Boolean, endPositions: (String) -> Boolean): Long {
-        val positions = graph.filterKeys(startPositions).keys
-        return positions.map {
+    override fun part1() = solve({ it == "AAA" }, { it == "ZZZ" })
+
+    override fun part2() = solve({ it.endsWith('A') }, { it.endsWith('Z') })
+
+    fun solve(startPositions: (String) -> Boolean, endPositions: (String) -> Boolean) =
+        graph.filterKeys(startPositions).keys.map {
             path.foldIndexed(it) { index, key, c ->
-                if(endPositions(key)){
+                if (endPositions(key)) {
                     return@map index.toLong()
                 }
                 when (c) {
                     'L' -> graph.getValue(key).first
                     else -> graph.getValue(key).second
                 }
-            }
-            error("")
+            }.let { error("should not exit loop") }
         }.reduce(::lcm)
-    }
 
-    fun lcm(a: Long, b: Long) = (a * b) / gcd(a, b)
+    private fun lcm(a: Long, b: Long) = (a * b) / gcd(a, b)
 
-    fun gcd(a: Long, b: Long): Long = when (b) {
+    private fun gcd(a: Long, b: Long): Long = when (b) {
         0L -> a
         else -> gcd(b, a % b)
     }
-
-    override fun part1() = solve({ it == "AAA" }, { it == "ZZZ" })
-
-    override fun part2() = solve({ it.endsWith('A') }, { it.endsWith('Z') })
 }
