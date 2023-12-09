17a2f69d5eaa05c1593eda18e4684ac36e2755fc
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-09 07:23:51 +0100

    smoll pp 2

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -8,9 +8,9 @@ fun main() {
     Day9.solve().let(::println)
 }
 
-fun List<Int>.next(): Int = lastOrNull()?.let { it + zipWithNext { a, b -> b - a }.next() } ?: 0
 object Day9 : Challenge() {
     private val parsed = input.lines().map { it.split(" ").map(String::toInt) }
+    fun List<Int>.next(): Int = lastOrNull()?.let { it + zipWithNext { a, b -> b - a }.next() } ?: 0
     override fun part1() = parsed.sumOf { it.next() }
     override fun part2() = parsed.sumOf { it.asReversed().next() }
 }
