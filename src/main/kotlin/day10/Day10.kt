be013fba0cfe625ebd3e85d81fc1807e2caab1d6
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-10 11:17:12 +0100

    pre-stokes solution

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -15,51 +15,40 @@ object Day10 : Challenge() {
     val west = 0 to -1
     val east = 0 to 1
 
-    enum class PipeType(val char: Char, vararg val directions: Point) {
-        NORTH_SOUTH('|', north, south),
-        NORTH_WEST('J', north, west),
-        NORTH_EAST('L', north, east),
-        EAST_WEST('-', east, west),
-        EAST_SOUTH('F', east, south),
-        SOUTH_WEST('7', south, west),
-        ALL('S', north, east, south, west),
-        NONE('.'),
-    }
-
-    data class Pipe(
-        val position: Point,
-        val pipeType: PipeType,
-    ) {
-        lateinit var neighbours: List<Pipe>
-    }
+    val pipeTypes = mapOf(
+        '|' to listOf(north, south),
+        'J' to listOf(north, west),
+        'L' to listOf(north, east),
+        '-' to listOf(east, west),
+        'F' to listOf(east, south),
+        '7' to listOf(south, west),
+        'S' to listOf(north, east, south, west),
+        '.' to emptyList(),
+    )
 
     operator fun Point.plus(other: Point) = first + other.first to second + other.second
+    lateinit var startPipe: Point
 
-    val pipes = buildMap {
+    val pipes = buildMap<Point, List<Point>> {
         input.lines().forEachIndexed { y, line ->
             line.forEachIndexed { x, c ->
-                put(y to x, PipeType.entries.first { it.char == c })
+                put(y to x, pipeTypes.getValue(c).map { it + (y to x) })
+                if (c == 'S') {
+                    startPipe = y to x
+                }
             }
         }
-    }.mapValues { (point, pipe) -> Pipe(point, pipe) }
-        .apply {
-            onEach { (point, pipe) ->
-                pipe.neighbours = pipe.pipeType.directions
-                    .map { it + point }
-                    .mapNotNull(::get)
-                    .filter { it.pipeType.directions.map { d -> d + it.position }.any { d -> d == pipe.position } }
-            }
-        }
+        mapValuesTo(this) { (point, neighbours) -> neighbours.filter { p -> get(p).orEmpty().any { it == point } } }
+    }
 
-    val startPipe = pipes.values.first { it.pipeType == PipeType.ALL }
     override fun part1(): Any? {
         val loop = findLoop(startPipe)
         return loop.size / 2
     }
 
-    fun findLoop(startPipe: Pipe) = buildSet {
-        generateSequence(startPipe to startPipe.neighbours.first()) { (p1, p2) ->
-            p2 to (p2.neighbours - p1)[0]
+    fun findLoop(startPipe: Point) = buildSet {
+        generateSequence(startPipe to pipes.getValue(startPipe).first()) { (p1, p2) ->
+            p2 to (pipes.getValue(p2) - p1)[0]
         }.forEach {
             if (!add(it.first)) {
                 return@buildSet
@@ -67,29 +56,30 @@ object Day10 : Challenge() {
         }
     }
 
-    fun Point.between(other: Point) = first + (other.first - first) / 2 to second + (other.second - second) / 2
+    private fun Point.between(other: Point) = first + (other.first - first) / 2 to second + (other.second - second) / 2
     operator fun Point.times(amount: Int) = first * amount to second * amount
 
-    fun Point.neighbours() = listOf(north, east, south, west).map { it + this }
+    private fun Point.neighbours() = listOf(north, east, south, west).map { it + this }
 
     override fun part2(): Any? {
         val loop = findLoop(startPipe)
-        val extendedLoop = loop.map { it.position }
-            .let { it + it.first() }
+        val extendedLoop = loop
+            .let { it.toList() + it.first() }
             .map { it * 2 }
             .zipWithNext { a, b -> listOf(a, a.between(b)) }
             .flatten()
         val workAreaHeight = (-2..input.lines().size * 2)
         val workAreaWidth = (-2..input.lines()[0].length * 2)
         val startPoint = -2 to -2
-        val visited = mutableSetOf<Point>()
+        val visited = mutableSetOf<Point>(startPoint)
         val recursiveFunction = DeepRecursiveFunction<Point, Unit> {
-            if (it.first in workAreaHeight && it.second in workAreaWidth && it !in extendedLoop && visited.add(it)) {
-                it.neighbours().forEach {
+            it.neighbours().forEach {
+                if (it.first in workAreaHeight && it.second in workAreaWidth && it !in extendedLoop && visited.add(it)) {
                     callRecursive(it)
                 }
             }
         }.invoke(startPoint)
+        visited.associate { it to "O" }.let(::printGraph)
         var sum = 0
         for (y in 0 until input.lines().size) {
             for (x in 0 until input.lines()[0].length) {
@@ -101,3 +91,22 @@ object Day10 : Challenge() {
         }
         return sum
     }
+    fun printGraph(map: Map<Point, Any?>) {
+        val yMin = map.minOf { it.key.first }
+        val yMax = map.maxOf { it.key.first }
+        val xMin = map.minOf { it.key.second }
+        val xMax = map.maxOf { it.key.second }
+        (yMin..yMax).forEach { y ->
+            (xMin..xMax).forEach { x ->
+                print(
+                    when (val value = map[y to x]) {
+                        null -> "I"
+                        else -> " "
+                    },
+                )
+            }
+            println()
+        }
+    }
+}
+
