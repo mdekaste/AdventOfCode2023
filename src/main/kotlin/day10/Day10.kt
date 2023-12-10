4bdbbbe062ccee18a427e124f4b5a01e6bb17657
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-10 13:07:59 +0100

    Stokes final (tm)

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -14,13 +14,12 @@ typealias Point = Pair<Int, Int>
 typealias Pipe = EnumSet<Compass>
 
 object Day10 : Challenge() {
-
-    val START_PIPE = EnumSet.allOf(Compass::class.java)
-    val EMPTY_PIPE = EnumSet.noneOf(Compass::class.java)
-    private fun Compass.otherSide() = Compass.entries[(Compass.entries.indexOf(this) + 2) % Compass.entries.size]
-    private fun Pipe.travel(compass: Compass): Compass = when (this) {
+    private val START_PIPE = EnumSet.allOf(Compass::class.java)
+    private val EMPTY_PIPE = EnumSet.noneOf(Compass::class.java)
+    private fun Compass.reciprocal() = Compass.entries[(Compass.entries.indexOf(this) + 2) % Compass.entries.size]
+    private fun Pipe.otherside(compass: Compass): Compass = when (this) {
         START_PIPE -> compass
-        else -> minus(compass.otherSide()).first()
+        else -> minus(compass.reciprocal()).first()
     }
     operator fun Point.plus(compass: Compass) = when (compass) {
         N -> first - 1 to second
@@ -29,46 +28,40 @@ object Day10 : Challenge() {
         W -> first to second - 1
     }
 
-    val moves: List<Compass> = run {
-        val grid = buildMap<Point, EnumSet<Compass>> {
-            input.lines().forEachIndexed { y, line ->
-                line.forEachIndexed { x, c ->
-                    put(
-                        y to x,
-                        when (c) {
-                            'L' -> EnumSet.of(N, E)
-                            '|' -> EnumSet.of(N, S)
-                            'J' -> EnumSet.of(N, W)
-                            'F' -> EnumSet.of(E, S)
-                            '-' -> EnumSet.of(E, W)
-                            '7' -> EnumSet.of(S, W)
-                            'S' -> START_PIPE
-                            else -> EMPTY_PIPE
-                        },
-                    )
+    val directions: List<Compass> = run {
+        val grid = input.lines().flatMapIndexed { y, line ->
+            line.mapIndexedNotNull { x, c ->
+                y to x to when (c) {
+                    'L' -> EnumSet.of(N, E)
+                    '|' -> EnumSet.of(N, S)
+                    'J' -> EnumSet.of(N, W)
+                    'F' -> EnumSet.of(E, S)
+                    '-' -> EnumSet.of(E, W)
+                    '7' -> EnumSet.of(S, W)
+                    'S' -> START_PIPE
+                    else -> EMPTY_PIPE
                 }
             }
-        }.withDefault { EMPTY_PIPE }
+        }.toMap().withDefault { EMPTY_PIPE }
         val startPipe = grid.entries.first { (_, s) -> s == START_PIPE }.key
-        val move = when {
+        val direction = when {
             E in grid.getValue(startPipe + W) -> W
             S in grid.getValue(startPipe + N) -> N
             else -> E
         }
-        generateSequence(startPipe to move) { (pos, move) ->
-            val nextPos = pos + move
-            nextPos.takeIf { it != startPipe }?.let { it to grid.getValue(nextPos).travel(move) }
-        }.map { it.second }.toList()
+        generateSequence(startPipe to direction) { (point, direction) ->
+            point.plus(direction).takeUnless(startPipe::equals)?.let { it to grid.getValue(it).otherside(direction) }
+        }.map { (_, direction) -> direction }.toList()
     }
 
-    override fun part1() = moves.size / 2
+    override fun part1() = directions.size / 2
 
-    override fun part2() = moves.fold(0 to 0) { (sum, d), compass ->
+    override fun part2() = directions.fold(0 to 0) { (sum, vector), compass ->
         when (compass) {
-            N -> sum to d + 1
-            S -> sum to d - 1
-            W -> sum - d to d
-            E -> sum + d to d
+            N -> sum + vector to vector
+            E -> sum to vector + 1
+            S -> sum - vector to vector
+            W -> sum to vector - 1
         }
     }.first.absoluteValue - part1() + 1
 }
