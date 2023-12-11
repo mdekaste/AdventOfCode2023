ff91c052a0f04339bfbece8dc1a5c191ac50d305
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-11 08:29:52 +0100

    first Pretty

--- src/main/kotlin/day11/Day11.kt
+++ src/main/kotlin/day11/Day11.kt
@@ -1,7 +1,7 @@
 package day11
 
 import Challenge
-import helpers.cartesianProduct
+import java.util.*
 import kotlin.math.abs
 
 // ktlint-disable no-wildcard-imports
@@ -12,53 +12,30 @@ fun main() {
 }
 
 object Day11 : Challenge() {
-    val parsed = input.lines().flatMapIndexed { y: Int, s: String ->
-        s.mapIndexedNotNull { x, c ->
-            when(c){
-                '#' -> y to x
-                else -> null
-            }
+    private val parsed = input.lines()
+        .flatMapIndexed { y: Int, s: String -> s.mapIndexedNotNull { x, c -> (y to x).takeIf { c == '#' } } }
+
+    private fun holes(selector: (Pair<Int, Int>) -> Int) = parsed.asSequence()
+        .map(selector)
+        .sorted()
+        .distinct()
+        .zipWithNext { a, b -> b to b - a - 1 }
+        .toMap(TreeMap())
+
+    override fun part1() = solve(2)
+    override fun part2() = solve(1000000)
+
+    private fun solve(expansionAmount: Int): Long {
+        val holesY = holes { it.first }
+        val holesX = holes { it.second }
+
+        val newPlaces = parsed.map { (y, x) ->
+            val holesBeforeY = holesY.headMap(y, true).values.sum()
+            val holesBeforeX = holesX.headMap(x, true).values.sum()
+            y + holesBeforeY * (expansionAmount - 1) to x + holesBeforeX * (expansionAmount - 1)
         }
-    }.toList()
-    override fun part1(): Any? {
-        print(parsed)
-        val sortedByY = parsed.groupBy { it.first }.toSortedMap()
-        val insertions = sortedByY.entries.zipWithNext { (y, _), (y2, _) -> y2 - y - 1 }.runningReduce { acc, i -> acc + i }.let { listOf(it.first()) + it + it.last() }
-        val parsed2 = sortedByY.values.zip(insertions).flatMap { (nodesByY, increases) -> nodesByY.map { it.first + increases to it.second } }.toList()
 
-        print(parsed2)
-        val sortedByX = parsed2.groupBy { it.second }.toSortedMap()
-        val insertions2 = sortedByX.entries.zipWithNext { (x, _), (x2, _) -> x2 - x - 1 }.runningReduce { acc, i -> acc + i }.let { listOf(it.first()) + it + it.last() }
-        val parsed3 = sortedByX.values.zip(insertions2).flatMap { (nodesByX, increases) -> nodesByX.map { it.first to it.second + increases } }.toList()
-        print(parsed3)
-
-        return parsed3.cartesianProduct(parsed3).filter { it.first != it.second }.map { (first, second) -> (abs(first.first - second.first) + abs(first.second - second.second)) }.sum() / 2
-    }
-
-    fun print(points: List<Pair<Int, Int>>){
-        println("---")
-        for(y in 0..points.maxOf { it.first }){
-            for(x in 0..points.maxOf { it.second }){
-                if(y to x in points){
-                    print('#')
-                } else {
-                    print('.')
-                }
-            }
-            println()
-        }
-        println("---")
-    }
-
-    override fun part2(): Any? {
-        val sortedByY = parsed.groupBy { it.first }.toSortedMap()
-        val insertions = sortedByY.entries.zipWithNext { (y, _), (y2, _) -> y2 - y - 1 }.map { it * (1000000 - 1) }.runningReduce { acc, i -> acc + i }.let { listOf(it.first()) + it + it.last() }
-        val parsed2 = sortedByY.values.zip(insertions).flatMap { (nodesByY, increases) -> nodesByY.map { it.first + increases to it.second } }.toList()
-
-        val sortedByX = parsed2.groupBy { it.second }.toSortedMap()
-        val insertions2 = sortedByX.entries.zipWithNext { (x, _), (x2, _) -> x2 - x - 1 }.map { it * (1000000 -1) }.runningReduce { acc, i -> acc + i }.let { listOf(it.first()) + it + it.last() }
-        val parsed3 = sortedByX.values.zip(insertions2).flatMap { (nodesByX, increases) -> nodesByX.map { it.first to it.second + increases } }.toList()
-
-        return parsed3.cartesianProduct(parsed3).filter { it.first != it.second }.map { (first, second) -> (abs(first.first - second.first).toLong() + abs(first.second - second.second)).toLong() }.sum() / 2
+        return newPlaces.flatMap { (y1, x1) -> newPlaces.map { (y2, x2) -> abs(y2 - y1) + abs(x2 - x1) } }
+            .sumOf { it.toLong() } / 2
     }
 }
