74e2ac659b52baa41f749940517595f51c60e930
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-12 08:26:39 +0100

    cleanup before work

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -9,25 +9,18 @@ fun main() {
 }
 
 object Day12 : Challenge() {
-    val parsed = input.lines().map {
-        it.split(" ").let { (a, b) ->
-            a.toList() to b.split(",").map { it.toInt() }
-        }
-    }
+    val parsed = input.lines().map { it.split(" ") }
 
-    override fun part1(): Any? {
-        return parsed.map { (springs, damaged) ->
-            solve(springs, damaged)
-        }.sum()
+    fun String.extend(amount: Int, separator: String) = List(amount) { this }.joinToString(separator)
+
+    fun solve(amount: Int) = parsed.sumOf { (springs, damages) ->
+        solve(
+            springs = springs.extend(amount, "?").toList(),
+            damaged = damages.extend(amount, ",").split(",").map(String::toInt),
+        )
     }
-    override fun part2(): Any? {
-        return parsed.map { (springs, damaged) ->
-            List(5) { springs }.joinToString("?") { it.joinToString("") }.toList() to List(5) { damaged }.flatten()
-        }
-            .map { (springs, damaged) ->
-                solve(springs, damaged)
-            }.sum()
-    }
+    override fun part1() = solve(1)
+    override fun part2() = solve(5)
 
     fun solve(springs: List<Char>, damaged: List<Int>): Long {
         buildMap {
@@ -37,7 +30,7 @@ object Day12 : Challenge() {
                     else -> {
                         val maxIndex = min(
                             springs.indexOf('#').takeIf { it >= 0 } ?: Int.MAX_VALUE,
-                            springs.size - damaged.drop(1).sum() - damaged.size + 1 - damage
+                            springs.size - damaged.drop(1).sum() - damaged.size + 1 - damage,
                         )
                         (0..maxIndex).sumOf { index ->
                             val piece = springs.subList(index, index + damage)
@@ -46,34 +39,6 @@ object Day12 : Challenge() {
                                 else -> 0
                             }
                         }
-                    }
-                }
-            }
-            return recursive(springs, damaged)
-        }
-    }
-
-    fun solve2(springs: List<Char>, damaged: List<Int>): Long {
-        buildMap<Pair<List<Char>, List<Int>>, Long> {
-            fun recursive(springs: List<Char>, damaged: List<Int>): Long = getOrPut(springs to damaged) {
-                val damage = damaged.firstOrNull() ?: return@getOrPut if (springs.none { it == '#' }) {
-                    1
-                } else {
-                    0
-                }
-                val firstIndex = 0
-                val maxCheck = springs.size - (damaged.drop(1).sum() + damaged.drop(1).size) - damage
-                val maxCheck2 = springs.indexOf('#').takeIf { it >= 0 } ?: Int.MAX_VALUE
-                val maxCheck3 = min(maxCheck, maxCheck2)
-                (firstIndex..maxCheck3).sumOf { index ->
-                    val piece = springs.subList(index, index + damage)
-                    if (piece.none { it == '.' } && springs.getOrNull(index + damage) != '#') {
-                        recursive(
-                            springs.drop(index + damage + 1),
-                            damaged.drop(1),
-                        )
-                    } else {
-                        0
                     }
                 }
             }
