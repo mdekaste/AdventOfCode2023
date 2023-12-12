325df616bdca04ebfadb7587a8be47f88c33a768
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-12 08:21:17 +0100

    initial cleanup

--- src/main/kotlin/day12/Day12.kt
+++ src/main/kotlin/day12/Day12.kt
@@ -17,15 +17,40 @@ object Day12 : Challenge() {
 
     override fun part1(): Any? {
         return parsed.map { (springs, damaged) ->
-            solve2(springs, damaged)
+            solve(springs, damaged)
         }.sum()
     }
     override fun part2(): Any? {
         return parsed.map { (springs, damaged) ->
-            List(5) { springs }.joinToString("?"){ it.joinToString("") }.toList() to List(5) { damaged }.flatten() }
+            List(5) { springs }.joinToString("?") { it.joinToString("") }.toList() to List(5) { damaged }.flatten()
+        }
             .map { (springs, damaged) ->
-                solve2(springs, damaged)
+                solve(springs, damaged)
             }.sum()
+    }
+
+    fun solve(springs: List<Char>, damaged: List<Int>): Long {
+        buildMap {
+            fun recursive(springs: List<Char>, damaged: List<Int>): Long = getOrPut(springs to damaged) {
+                when (val damage = damaged.firstOrNull()) {
+                    null -> if (springs.none { it == '#' }) 1 else 0
+                    else -> {
+                        val maxIndex = min(
+                            springs.indexOf('#').takeIf { it >= 0 } ?: Int.MAX_VALUE,
+                            springs.size - damaged.drop(1).sum() - damaged.size + 1 - damage
+                        )
+                        (0..maxIndex).sumOf { index ->
+                            val piece = springs.subList(index, index + damage)
+                            when (piece.none { it == '.' } && springs.getOrNull(index + damage) != '#') {
+                                true -> recursive(springs.drop(index + damage + 1), damaged.drop(1))
+                                else -> 0
+                            }
+                        }
+                    }
+                }
+            }
+            return recursive(springs, damaged)
+        }
     }
 
     fun solve2(springs: List<Char>, damaged: List<Int>): Long {
