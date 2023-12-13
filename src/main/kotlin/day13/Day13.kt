2a59ced1eb5114f0a318c00544eda143a9fe2697
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-13 09:06:08 +0100

    Can I haz functional programming?

--- src/main/kotlin/day13/Day13.kt
+++ src/main/kotlin/day13/Day13.kt
@@ -2,89 +2,31 @@ package day13
 
 import Challenge
 import helpers.splitOnEmpty
-import kotlin.math.min
+import java.util.*
 
 fun main() {
     Day13.part1().let(::println)
     Day13.part2().let(::println)
 }
 
+typealias Grid = List<String>
+
+fun Grid.findReflectionIndex(not: Int = -1): Int? = indexOfReflection().map(100::times).plus(transpose().indexOfReflection()).firstOrNull { it != not }
+fun Grid.transpose() = first().indices.map { x -> indices.joinToString("") { y -> get(y).get(x).toString() } }
+fun Grid.indexOfReflection() = indices.drop(1).filter(::splitsAlong)
+fun Grid.splitsAlong(index: Int): Boolean = subList(0, index).matches(subList(index, size))
+fun List<String>.matches(other: List<String>) = asReversed().zip(other).all { (a, b) -> a == b }
+
+fun Grid.unSmudge(): List<Grid> = flatMapIndexed { y, s -> s.indices.map { x -> unSmudge(y, x) } }
+fun Grid.unSmudge(y: Int, x: Int): Grid = take(y) + get(y).unSmudge(x) + drop(y + 1)
+fun String.unSmudge(x: Int) = take(x) + get(x).unSmudge() + drop(x + 1)
+fun Char.unSmudge() = if (this == '#') '.' else '#'
+
 object Day13 : Challenge() {
-    val parsed = input.splitOnEmpty().map { board ->
-        board.lines()
-    }
-    override fun part1(): Any? {
-        return parsed.map { it.toReflectionIndex() }.sum()
-    }
-
-    fun List<String>.toReflectionIndex(not: Int = -1): Int {
-        return (1 until size).mapNotNull {
-            val above = subList(0, it)
-            val under = subList(it, size)
-            if (above.matches(under)) {
-                100 * it
-            } else {
-                null
-            }
-        }.firstOrNull { it != not } ?: this.flip(not)
-    }
-
-    fun List<String>.flip(not: Int = -1): Int {
-        val result = List(first().length) { List(size) { ' ' }.toMutableList() }
-        for (x in 0 until size) {
-            for (y in 0 until first().length) {
-                result[y][x] = get(x).get(y)
-            }
-        }
-        val actual = result.map { it.joinToString("") }
-        return (
-            (1 until actual.size).mapNotNull {
-                val above = actual.subList(0, it)
-                val under = actual.subList(it, actual.size)
-                if (above.matches(under)) {
-                    it
-                } else {
-                    null
-                }
-            }.firstOrNull{ it != not } ?: 0
-            )
-    }
-
-    fun List<String>.matches(other: List<String>): Boolean {
-        for (i in 0..<min(size, other.size)) {
-            val thisIndex = size - 1 - i
-            if (get(thisIndex) != other[i]) {
-                return false
-            }
-        }
-        return true
-    }
-
-    override fun part2(): Any? {
-        return parsed.map { boards ->
-            boards.fixSmudge().let { (source, others) ->
-                val sourceReflectionIndex = source.toReflectionIndex()
-                val x = others.map { it.toReflectionIndex(sourceReflectionIndex) }.filter { it != 0 }.firstOrNull() { it != sourceReflectionIndex } ?: -1
-                x
-            }
-        }.sum()
-    }
-
-    fun List<String>.fixSmudge(): Pair<List<String>, List<List<String>>> {
-        val result = mutableListOf<List<String>>()
-        for (y in 0 until size) {
-            for (x in 0 until first().length) {
-                val resultingSet = map { it.toMutableList() }.toList()
-                result += resultingSet.apply {
-                    val char = this[y][x]
-                    if (char == '#') {
-                        resultingSet[y][x] = '.'
-                    } else {
-                        resultingSet[y][x] = '#'
-                    }
-                }.map { it.joinToString("") }
-            }
-        }
-        return this to result
-    }
+    val parsed: List<Grid> = input.splitOnEmpty().map(String::lines)
+    val reflectionIndexes = parsed.mapNotNull(Grid::findReflectionIndex)
+    override fun part1() = reflectionIndexes.sum()
+    override fun part2() = reflectionIndexes.zip(parsed.map(Grid::unSmudge)) { from, candidates ->
+        candidates.firstNotNullOf { it.findReflectionIndex(from) }
+    }.sum()
 }
