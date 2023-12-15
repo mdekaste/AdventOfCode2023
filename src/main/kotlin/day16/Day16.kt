e8d0c563ce2071ab46b90282e51c9a1d8b851b1b
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-15 19:57:30 +0100

    prepare day 16

---
+++ src/main/kotlin/day16/Day16.kt
@@ -0,0 +1,18 @@
+package day16
+
+import Challenge
+
+fun main() {
+    Day16.part1().let(::println)
+    Day16.part2().let(::println)
+}
+
+object Day16 : Challenge() {
+    override fun part1(): Any? {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(): Any? {
+        TODO("Not yet implemented")
+    }
+}
