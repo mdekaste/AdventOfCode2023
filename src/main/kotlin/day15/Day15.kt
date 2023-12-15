8cc776b9c0ccb26ce132ac891a6ff1a18999f972
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-15 08:27:16 +0100

    Day 15

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,31 +1,24 @@
 package day15
 
 import Challenge
-import kotlin.time.measureTimedValue
 
 fun main() {
-    repeat(10000) {
-        val day = Day15()
-        day.part1()
-        day.part2()
-    }
-    val (day, time) = measureTimedValue { Day15() }
-    println("Reading input takes: $time")
-    val (part1, timePart1) = measureTimedValue { day.part1() }
-    println("Part 1 is $part1 and takes $timePart1")
-    val (part2, timePart2) = measureTimedValue { day.part2() }
-    println("Part 2 is $part2 and takes $timePart2")
+    Day15.part1().let(::println)
+    Day15.part2().let(::println)
 }
 
-class Day15 : Challenge() {
+object Day15 : Challenge() {
     val parsed = input.split(",")
 
     private fun hash(input: String): Int = input.fold(0) { hash, c -> ((hash + c.code) * 17) % 256 }
     override fun part1() = parsed.map(::hash).sum()
 
-    override fun part2() = parsed.fold(MutableList(256) { mutableMapOf<String, Int>() }) { acc, line ->
+    override fun part2() = parsed.fold(Array(256) { mutableMapOf<String, Int>() }) { acc, line ->
         val (value, focalLength) = line.split("=", "-")
-        if ("-" in line) acc[hash(value)] -= value else acc[hash(value)][value] = focalLength.toInt()
+        when {
+            "-" in line -> acc[hash(value)] -= value
+            else -> acc[hash(value)][value] = focalLength.toInt()
+        }
         acc
     }.withIndex().sumOf { (i, map) -> (i + 1) * map.values.withIndex().sumOf { (j, value) -> (j + 1) * value } }
 }
