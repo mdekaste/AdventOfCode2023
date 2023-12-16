44c18dd0b5491afa757c1b57ec5c1c77b27be1ff
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-16 07:42:47 +0100

    initial cleanup

--- src/main/kotlin/Challenge.kt
+++ src/main/kotlin/Challenge.kt
@@ -2,6 +2,7 @@ import java.io.File
 import kotlin.time.measureTimedValue
 
 typealias Point = Pair<Int, Int>
+
 abstract class Challenge(
     val name: String? = null,
 ) {
@@ -17,17 +18,21 @@ abstract class Challenge(
         }
         return measureTimedValue { listOf(name, part1(), part2()) }
     }
-    val NORTH = -1 to 0
-    val EAST = 0 to 1
-    val SOUTH = 1 to 0
-    val WEST = 0 to -1
-    val NORTH_EAST = -1 to 1
-    val SOUTH_EAST = 1 to 1
-    val SOUTH_WEST = 1 to -1
-    val NORTH_WEST = -1 to -1
-    val ORIGIN = 0 to 0
-    operator fun Point.plus(other: Point) = first + other.first to second + other.second
-    operator fun Point.minus(other: Point) = first - other.first to second - other.second
-    fun Point.neighbours4() = listOf(NORTH, EAST, SOUTH, WEST).map { it + this }
-    fun Point.neighbours8() = listOf(NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST).map { it + this }
+
+    companion object {
+        val NORTH = -1 to 0
+        val EAST = 0 to 1
+        val SOUTH = 1 to 0
+        val WEST = 0 to -1
+        val NORTH_EAST = -1 to 1
+        val SOUTH_EAST = 1 to 1
+        val SOUTH_WEST = 1 to -1
+        val NORTH_WEST = -1 to -1
+        val ORIGIN = 0 to 0
+        operator fun Point.plus(other: Point) = first + other.first to second + other.second
+        operator fun Point.minus(other: Point) = first - other.first to second - other.second
+        fun Point.neighbours4() = listOf(NORTH, EAST, SOUTH, WEST).map { it + this }
+        fun Point.neighbours8() =
+            listOf(NORTH, NORTH_EAST, EAST, SOUTH_EAST, SOUTH, SOUTH_WEST, WEST, NORTH_WEST).map { it + this }
+    }
 }
--- src/main/kotlin/day16/Day16.kt
+++ src/main/kotlin/day16/Day16.kt
@@ -1,18 +1,100 @@
 package day16
 
 import Challenge
+import Challenge.Companion.EAST
+import Challenge.Companion.NORTH
+import Challenge.Companion.SOUTH
+import Challenge.Companion.WEST
+import Challenge.Companion.plus
+import Point
 
 fun main() {
-    Day16.part1().let(::println)
-    Day16.part2().let(::println)
+    Day16().part1().let(::println)
+    Day16().part2().let(::println)
 }
 
-object Day16 : Challenge() {
-    override fun part1(): Any? {
-        TODO("Not yet implemented")
+class Day16 : Challenge() {
+
+    private val parsed = input.lines()
+    private val graph = buildMap {
+        parsed.forEachIndexed { y, s ->
+            s.forEachIndexed { x, c ->
+                put(y to x, Node(y to x, c, this))
+            }
+        }
     }
 
-    override fun part2(): Any? {
-        TODO("Not yet implemented")
+    override fun part1() = solve(listOf((0 to -1) to (0 to 0)))
+    override fun part2() = solve(generateOptions(parsed))
+
+    private fun generateOptions(parsed: List<String>) = buildList {
+        val height = parsed.size
+        val width = parsed.first().length
+        for (x in parsed.first().indices) {
+            add((-1 to x) to (0 to x))
+            add((height to x) to (height - 1 to x))
+        }
+        for (y in parsed.indices) {
+            add((y to -1) to (y to 0))
+            add((y to width) to (y to width - 1))
+        }
+    }
+
+    fun solve(startPoints: List<Pair<Point, Point>>): Int = startPoints.maxOf { (s1, s2) ->
+        val curSet = mutableListOf(s1 to graph.getValue(s2))
+        val visited = mutableSetOf(s1 to graph.getValue(s2))
+        while (curSet.isNotEmpty()) {
+            val (from, cur) = curSet.removeLast()
+            cur.reflections[from]?.forEach {
+                if (visited.add(cur.pos to it)) {
+                    curSet.add(cur.pos to it)
+                }
+            }
+        }
+        visited.map { it.second }.distinct().size
+    }
+}
+
+class Node(
+    val pos: Point,
+    val character: Char,
+    graph: Map<Point, Node>
+) {
+    val reflections: Map<Point, List<Node>> by lazy {
+        val neighbours = listOf(NORTH, SOUTH, WEST, EAST)
+            .filter { graph.containsKey(it + pos) }
+            .associateWith { graph.getValue(it + pos) }
+
+        listOf(NORTH, SOUTH, WEST, EAST).associateBy({ it + pos }, {
+            when (it) {
+                WEST -> when (character) {
+                    '.', '-' -> listOfNotNull(neighbours[EAST])
+                    '|' -> listOfNotNull(neighbours[NORTH], neighbours[SOUTH])
+                    '/' -> listOfNotNull(neighbours[NORTH])
+                    else -> listOfNotNull(neighbours[SOUTH])
+                }
+
+                EAST -> when (character) {
+                    '.', '-' -> listOfNotNull(neighbours[WEST])
+                    '|' -> listOfNotNull(neighbours[NORTH], neighbours[SOUTH])
+                    '/' -> listOfNotNull(neighbours[SOUTH])
+                    else -> listOfNotNull(neighbours[NORTH])
+                }
+
+                NORTH -> when (character) {
+                    '.', '|' -> listOfNotNull(neighbours[SOUTH])
+                    '-' -> listOfNotNull(neighbours[EAST], neighbours[WEST])
+                    '/' -> listOfNotNull(neighbours[WEST])
+                    else -> listOfNotNull(neighbours[EAST])
+                }
+
+                else -> when (character) {
+                    '.', '|' -> listOfNotNull(neighbours[NORTH])
+                    '-' -> listOfNotNull(neighbours[EAST], neighbours[WEST])
+                    '/' -> listOfNotNull(neighbours[EAST])
+                    else -> listOfNotNull(neighbours[WEST])
+                }
+            }
+        })
     }
 }
