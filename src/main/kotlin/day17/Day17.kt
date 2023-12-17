a083cf2932dbabe49d7cadad7d28a84e1134de8c
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-17 16:06:22 +0100

    Moved some stuff

--- src/main/kotlin/day17/Day17.kt
+++ src/main/kotlin/day17/Day17.kt
@@ -18,49 +18,49 @@ object Day17 : Challenge() {
     private val parsed = input.lines()
         .flatMapIndexed { y, s -> s.mapIndexed { x, c -> y to x to c.digitToInt() } }
         .toMap()
+
     private val startPoint = 0 to 0
     private val endPoint = (parsed.maxOf { it.key.first }) to (parsed.maxOf { it.key.second })
 
-    data class State(
-        val point: Point = startPoint,
-        val direction: Point,
-        val moveMemory: Int = 1,
-        val score: Int = 0
-    )
+    data class State(val point: Point = startPoint, val direction: Point, val forwardCount: Int = 1) {
+        var score: Int = 0
+        constructor(point: Point, dir: Point, forwardCount: Int, score: Int) : this(point, dir, forwardCount) {
+            this.score = score
+        }
+    }
 
     override fun part1() = solve(0, 3)
     override fun part2() = solve(4, 10)
 
     fun solve(minimalForward: Int, maximumForward: Int): Int {
-        val visited = mutableSetOf(
-            Triple(startPoint, EAST, 1),
-            Triple(startPoint, SOUTH, 1)
-        )
-        val queue = PriorityQueue(compareBy(State::score, State::moveMemory)).apply {
-            add(State(startPoint, EAST, 1, 0))
-            add(State(startPoint, SOUTH, 1, 0))
+        val stateOne = State(direction = EAST)
+        val stateTwo = State(direction = SOUTH)
+        val visited = mutableSetOf(stateOne, stateTwo)
+        val queue = PriorityQueue(compareBy(State::score, State::forwardCount)).apply {
+            add(stateOne)
+            add(stateTwo)
         }
+
         fun add(state: State) {
-            if (visited.add(Triple(state.point, state.direction, state.moveMemory))) {
+            if (visited.add(state)) {
                 queue.offer(state)
             }
         }
-        while (queue.isNotEmpty()) {
-            val (point, direction, forwardCount, score) = queue.poll()
-            val nextPoint = point + direction
-            val nextScore = score + (parsed[nextPoint] ?: continue)
-            if (nextPoint == endPoint) {
+        while (true) {
+            val state = queue.poll()
+            val nextPoint = state.point + state.direction
+            val nextScore = state.score + (parsed[nextPoint] ?: continue)
+            if (nextPoint == endPoint && state.forwardCount >= minimalForward) {
                 return nextScore
             }
-            if (forwardCount < maximumForward) {
-                add(State(nextPoint, direction, forwardCount + 1, nextScore))
+            if (state.forwardCount < maximumForward) {
+                add(State(nextPoint, state.direction, state.forwardCount + 1, nextScore))
             }
-            if (forwardCount >= minimalForward) {
-                direction.perpendicular().forEach {
+            if (state.forwardCount >= minimalForward) {
+                state.direction.perpendicular().forEach {
                     add(State(nextPoint, it, 1, nextScore))
                 }
             }
         }
-        error("could not determine score at end")
     }
 }
