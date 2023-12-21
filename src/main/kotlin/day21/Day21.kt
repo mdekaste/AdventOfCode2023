fd181575f4f66688ab73c926162b2fd0c753fbb6
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-21 19:10:19 +0100

    Ok, it works generally now

--- src/main/kotlin/day21/Day21.kt
+++ src/main/kotlin/day21/Day21.kt
@@ -10,8 +10,6 @@ fun main(){
     Day21.part2().let(::println)
 }
 
-typealias Frontier = Pair<Set<Point>, Set<Point>>
-typealias PointCount = Map<Point, Long>
 object Day21 : Challenge() {
     val parsed = input.lines().flatMapIndexed { y, c ->
         c.mapIndexed { x, c -> y to x to c }
@@ -46,43 +44,60 @@ object Day21 : Challenge() {
         val previousScore: Long,
         val previousFrontier: Set<Point>,
         val currentFrontier: Set<Point>,
-        val score: Pair<Long, Long>,
+        val score: Long,
     ) {
-        fun memoryState() = currentFrontier.map { it.pq() }.toSet()
+        val memoryState by lazy { currentFrontier.map { it.pq() }.toSet() }
     }
 
-    fun solveGeneric(depth: Long): Long {
-        val stateSequence = sequence<State>{
-            var state = State(0, emptySet(), setOf(startPoint), 0L to 1L)
-            while(true){
-                yield(state)
-                val newFrontier = mutableSetOf<Point>()
-                for(point in state.currentFrontier){
-                    for(neighbour in point.cardinals()){
-                        val puPoint = neighbour.pq()
-                        if(parsed[puPoint] != '#' && neighbour !in state.previousFrontier){
-                            newFrontier.add(neighbour)
-                        }
+    private val states = sequence<State>{
+        var state = State(0, emptySet(), setOf(startPoint), 1L)
+        while(true){
+            yield(state)
+            val newFrontier = mutableSetOf<Point>()
+            for(point in state.currentFrontier){
+                for(neighbour in point.cardinals()){
+                    val puPoint = neighbour.pq()
+                    if(parsed[puPoint] != '#' && neighbour !in state.previousFrontier){
+                        newFrontier.add(neighbour)
                     }
                 }
-                val newScore = state.previousScore to newFrontier.size.toLong()
-                state = State(state.score.first + state.score.second, state.currentFrontier, newFrontier, newScore)
             }
+            state = State(state.score, state.currentFrontier, newFrontier, state.previousScore + newFrontier.size)
         }
-        val memory = mutableMapOf<Set<Point>, MutableList<Pair<Long, Long>>>()
-        for(state in stateSequence){
-            val stateMemory = state.memoryState()
-            if(memory[stateMemory]?.size == 3){
-                memory.map { it.value }.forEach { println(it) }
-                // do some magic wolfram alpha kekw maybe later ill fix
-                break
-            } else {
-                memory[stateMemory] = memory.getOrPut(stateMemory){ mutableListOf<Pair<Long, Long>>() }.apply { add(state.score) }
-            }
-        }
-        return 0L
     }
 
-    override fun part2() = solveGeneric(100000L)
+    fun solveGeneric(depth: Int): Long {
+        val memory = mutableMapOf<Set<Point>, MutableList<Long>>()
+        for(state in states){
+            when(memory[state.memoryState]?.size){
+                3 -> break
+                else -> memory[state.memoryState] = memory.getOrPut(state.memoryState){ mutableListOf() }.apply { add(state.score) }
+            }
+        }
+
+        val answers = memory.values.toList()
+        val indexOfCycles = answers.indexOfFirst { it.size == 3 }
+        val justValues = answers.subList(0, indexOfCycles).map { it.first() }
+
+        return when(depth){
+            in 0 until indexOfCycles -> justValues[depth]
+            else -> {
+                val formulas = answers.subList(indexOfCycles, answers.size).map { (y1, y2, y3) -> toQuadratic(y1, y2, y3) }
+                val sizeOfCycle = formulas.size
+                val iteration = (depth - indexOfCycles) / sizeOfCycle + 1
+                val indexInsideCycle = (depth - indexOfCycles) % sizeOfCycle
+                formulas[indexInsideCycle].let { (a, b, c) -> a * iteration * iteration + b * iteration + c }
+            }
+        }
+    }
+
+    fun toQuadratic(y1: Long, y2: Long, y3: Long): Triple<Long, Long, Long>{
+        val a = ((y3 - y2) - (y2 - y1)) / 2
+        val b = (y2 - y1) - 3 * a
+        val c = y1 - (a + b)
+        return Triple(a, b, c)
+    }
+
+    override fun part2() = solveGeneric(26501365)
 
 }
