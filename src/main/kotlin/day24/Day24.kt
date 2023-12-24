52d9f8746fef614f9ecabc38656bfd20b08be316
Michael de Kaste <michael.de.kaste@topicus.nl>
2023-12-24 08:25:47 +0100

    im about to give up fam

--- src/main/kotlin/day24/Day24.kt
+++ src/main/kotlin/day24/Day24.kt
@@ -1,8 +1,9 @@
 package day24
 
 import Challenge
+import java.math.BigDecimal
 
-fun main(){
+fun main() {
     Day24.part1().let(::println)
     Day24.part2().let(::println)
 }
@@ -10,61 +11,81 @@ fun main(){
 object Day24 : Challenge() {
     val parsed = input.lines().map {
         it.split(" @ ").let { (a,b) ->
-            a.split(", ").map { it.toLong() } to b.split(", ").map { it.toLong() }
+            val (x,y,z) = a.split(", ").map { it.toLong() }
+            val (xDiff, yDiff, zDiff) = b.split(", ").map { it.toLong() }
+            Input(x,y,z,xDiff,yDiff,zDiff)
         }
     }
 
-    //const val LOWERBOUND = 200000000000000L
-    //const val UPPERBOUND = 400000000000000L
+    val LOWERBOUND = 200000000000000.0.toBigDecimal()
+    val UPPERBOUND = 400000000000000.0.toBigDecimal()
 
-    const val LOWERBOUND = 0.0
-    const val UPPERBOUND = Double.MAX_VALUE
+//    const val LOWERBOUND = 7.0
+//    const val UPPERBOUND = 27.0
 
     override fun part1(): Any? {
-        val equations = parsed.map { it.toEquation() }
-        val collisions = equations.zip(parsed).flatMapIndexed { index: Int, pair1 ->
-            equations.zip(parsed).mapIndexedNotNull { index2, pair2 ->
-                if(index == index2){
-                    null
-                } else {
-                    collides(pair1.first, pair2.first, pair1.second, pair2.second)
-                }
-            }
-        }
-        return collisions.count { it }
+        val toCheck = parsed.flatMapIndexed { index: Int, input: Input -> parsed.drop(index + 1).map { input to it } }
+        return toCheck.map { (first, second) -> intersects(first, second) }.count { it }
     }
 
-     fun collides(first: Pair<Long, Long>, second: Pair<Long, Long>, equation: Pair<List<Long>, List<Long>>, equation2: Pair<List<Long>, List<Long>>): Boolean {
-         val a1 = -first.first
-         val b1 = 1
-         val c1 = -first.second
+    fun intersects(input1: Input, input2: Input): Boolean {
+        var x: BigDecimal
+        var y: BigDecimal
+        if(input1.equation == input2.equation){
+            val xGrowth = input1.x < input2.x && input1.xDiff > 0 && input2.xDiff < 0
+            val xGrowth2 = input1.x > input2.x && input1.xDiff < 0 && input2.xDiff > 0
+            val yGrowth = input1.y < input2.y && input1.yDiff > 0 && input2.yDiff < 0
+            val yGrowth2 = input1.y > input2.y && input1.yDiff < 0 && input2.yDiff > 0
+            if((xGrowth || xGrowth2) && (yGrowth || yGrowth2)){
+                x = ((input1.x + input2.x) / 2L).toBigDecimal()
+                y = ((input1.y + input2.y) / 2L).toBigDecimal()
+            }
+            return false
+        } else if(input1.equation.m == input2.equation.m) {
+            return false
+        } else {
+            x = (input2.equation.b - input1.equation.b) / (input1.equation.m - input2.equation.m)
+            y = (input1.equation.m * x + input1.equation.b)
+        }
 
-         val a2 = -second.first
-         val b2 = 1
-         val c2 = -second.second
-         try {
-             val yIntersect = ((c1 * a2) - (c2 * a1)) / ((a1 * b2) - (a2 * b1).toDouble())
-             val xIntersect = ((b1 * c2) - (b2 * c1)) / ((a1 * b2) - (a2 * b1).toDouble())
-             val ybound1 = equation.first[1].toDouble()
-             val xbound1 = equation.first[0].toDouble()
-             val ybound2 = equation2.first[1].toDouble()
-             val xbound2 = equation2.first[0].toDouble()
-             val rangey1 = if(equation.second[1] < 0) LOWERBOUND..ybound1 else ybound1..UPPERBOUND
-             val rangex1 = if(equation.second[0] < 0) LOWERBOUND..xbound1 else xbound1..UPPERBOUND
-             val rangey2 = if(equation2.second[1] < 0) LOWERBOUND..ybound2 else ybound2..UPPERBOUND
-             val rangex2 = if(equation2.second[0] < 0) LOWERBOUND..xbound2 else xbound2..UPPERBOUND
-             return yIntersect in rangey1 && xIntersect in rangex1 && yIntersect in rangey2 && xIntersect in rangex2
-         } catch (exception: Exception) {
-             return false
-         }
-     }
+        if(x < LOWERBOUND || x > UPPERBOUND || y < LOWERBOUND || y > UPPERBOUND){
+            return false
+        }
+        if(input1.xDiff >= 0 && x <= input1.x.toBigDecimal()){
+            return false
+        }
+        if(input2.xDiff >= 0 && x <= input2.x.toBigDecimal()){
+            return false
+        }
+        if(input1.yDiff >= 0 && y <= input1.y.toBigDecimal()){
+            return false
+        }
+        if(input2.yDiff >= 0 && y <= input2.y.toBigDecimal()){
+            return false
+        }
+        return true
+    }
 
-    fun Pair<List<Long>, List<Long>>.toEquation(): Pair<Long, Long> {
-        val point1 = first[1] to first[0]
-        val point2 = (first[1] + second[1]) to (first[0] + second[0])
-        val slope = (point2.first - point1.first) / (point2.second - point1.second)
-        val b = point1.first - slope * point1.second
-        return slope to b
+    data class Input(
+        val x: Long,
+        val y: Long,
+        val z: Long,
+        val xDiff: Long,
+        val yDiff: Long,
+        val zDiff: Long
+    ) {
+        data class Equation(
+            val m: BigDecimal,
+            val b: BigDecimal
+        )
+
+        val equation = run {
+            val x2 = x + xDiff
+            val y2 = y + yDiff
+            val m = (y2 - y).toBigDecimal() / (x2 - x).toBigDecimal()
+            val b = y.toBigDecimal() - m * x.toBigDecimal()
+            Equation(m, b)
+        }
     }
 
     override fun part2(): Any? {
